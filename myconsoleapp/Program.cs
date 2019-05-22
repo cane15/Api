7a5f95570035c271153ac63a3cbd6e4cfdb60f90
cane15 <cane15@users.noreply.github.com>
2019-05-22 16:06:04 +0200

    Add project files.

---
+++ myconsoleapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Fahim!");
+            Console.ReadLine();
+        }
+    }
+}
+ 
